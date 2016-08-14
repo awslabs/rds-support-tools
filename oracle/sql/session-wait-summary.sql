b25fc2ec86de3afc3f1a487f118e64ed2e90fcef
deanah <holmer@amazon.com>
2016-08-14 17:47:38 +0000

    8/14/2106 deanah added oracle/sql/session-wait-summary.sql

---
+++ oracle/sql/session-wait-summary.sql
@@ -0,0 +1,26 @@
+/*
+Copyright [first edit year]-[latest edit year] Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+*/ 
+
+
+COL event FORM a40
+COL seq# FORM 999999
+SET WRAP OFF
+SET LINESIZE 110
+SET PAGES 80
+
+SELECT
+  event,
+  count(*)
+FROM v$session_wait
+GROUP BY event
+ORDER BY count(*) DESC
+; 
+
