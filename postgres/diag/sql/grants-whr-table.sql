8b13e6f140914d6e90f69f644d8ac37389fbae9b
David Thomas <david@digitaldogma.org>
2016-11-23 14:07:10 +0000

    fixed a typo and updated the README with the new script name.

--- postgres/diag/sql/grants-whr-table.sql
+++ postgres/diag/sql/grants-whr-table.sql
@@ -14,7 +14,9 @@
  * and limitations under the License.
 */
 
+/* Run as: psql -v tbl_name=mytable  -f grants-whr-table.sql */
+
 SELECT grantee, string_agg(privilege_type, ', ') AS privileges
 FROM information_schema.role_table_grants
-WHERE table_name=':tbl_name'
+WHERE table_name=:'tbl_name'
 GROUP BY grantee;
