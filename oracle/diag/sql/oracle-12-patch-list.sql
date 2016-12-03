ea080d1906b9d0824c38f1f0bc0c7c40506afa2e
marozzin <marozzin@amazon.com>
2016-12-03 11:44:24 +0000

    Minor changes to include license blurb and exception for SE1 in readme file

--- oracle/diag/sql/oracle-12-patch-list.sql
+++ oracle/diag/sql/oracle-12-patch-list.sql
@@ -1,4 +1,19 @@
----The following query can be used in RDS Oracle 12c engine versions to list all patches installed
+/*
+ *  Copyright 2016 Amazon.com, Inc. or its affiliates.
+ *  All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License").
+ *  You may not use this file except in compliance with the License.
+ *  A copy of the License is located at
+ *
+ *      http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file.
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+*/
+
 set pages 1000 lines 20000
 
 with a as (select SYS.dbms_qopatch.get_opatch_lsinventory patch_output from dual)
