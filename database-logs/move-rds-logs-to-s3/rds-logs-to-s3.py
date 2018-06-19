2b06189e234c8d5625a27e0f185bf3e9d791f71a
Kiran-Janarthan <kiransg@amazon.com>
2018-06-19 20:49:16 +0000

    Move RDS logs to S3

--- database-logs/move-rds-logs-to-s3/rds-logs-to-s3.py
+++ database-logs/move-rds-logs-to-s3/rds-logs-to-s3.py
@@ -1,4 +1,3 @@
-
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
@@ -14,16 +13,16 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
+from __future__ import print_function
 import boto3, botocore, sys
 from datetime import datetime
 
 def print_usage():
-	print "Usage:\n<script_name>\n" + \
+	print ("Usage:\n<script_name>\n" + \
 				"\t--bucketname <S3 Buket Name>\n" + \
 				"\t--rdsinstancename <RDS Instance Name>\n" + \
 				"\t[--lognameprefix <Log Name Prefix>]\n" + \
-				"\t--region <Region>"
+				"\t--region <Region>")
 
 def parse_args(args):
 	global config
@@ -44,7 +43,7 @@ def parse_args(args):
 			config['Region'] = args[i + 1]
 			i += 1
 		else:
-			print "ERROR: Invalid command line argument " +arg + "/ No value specified"
+			print ("ERROR: Invalid command line argument " +arg + "/ No value specified")
 			print_usage()
 			return False
 		i += 1
@@ -67,7 +66,7 @@ def copy_logs_from_RDS_to_S3():
 			logNamePrefix = ""
 		configFileName = RDSInstanceName + "/" + "backup_config"
 	else:
-		print "ERROR: Values for the required field not specified"
+		print ("ERROR: Values for the required field not specified")
 		print_usage()
 		return 
 
@@ -84,10 +83,10 @@ def copy_logs_from_RDS_to_S3():
 	except botocore.exceptions.ClientError as e:
 		error_code = int(e.response['ResponseMetadata']['HTTPStatusCode'])
 		if error_code == 404:
-			print "Error: Bucket name provided not found"
+			print ("Error: Bucket name provided not found")
 			return
 		else:
-			print "Error: Unable to access bucket name, error: " + e.response['Error']['Message']
+			print ("Error: Unable to access bucket name, error: " + e.response['Error']['Message'])
 			return
 
     # get the config file, if the config isn't present this is the first run
@@ -98,9 +97,9 @@ def copy_logs_from_RDS_to_S3():
 	except botocore.exceptions.ClientError as e:
 		error_code = int(e.response['ResponseMetadata']['HTTPStatusCode'])
 		if error_code == 404:
-			print "It appears this is the first log import, all files will be retrieved from RDS"
+			print ("It appears this is the first log import, all files will be retrieved from RDS")
 		else:
-			print "Error: Unable to access config file, error: " + e.response['Error']['Message']
+			print ("Error: Unable to access config file, error: " + e.response['Error']['Message'])
 			return
 		
 	# copy the logs in batches to s3
@@ -116,7 +115,7 @@ def copy_logs_from_RDS_to_S3():
 
 		# copy the logs in this batch
 		for dbLog in dbLogs['DescribeDBLogFiles']:
-			print "FileNumber: ", copiedFileCount + 1
+			print ("FileNumber: ", copiedFileCount + 1)
 			print("Downloading log file: %s found and with LastWritten value of: %s " % (dbLog['LogFileName'],dbLog['LastWritten']))
 			if int(dbLog['LastWritten']) > lastWrittenThisRun:
 				lastWrittenThisRun = int(dbLog['LastWritten'])
@@ -130,11 +129,11 @@ def copy_logs_from_RDS_to_S3():
 					logFile = RDSclient.download_db_log_file_portion(DBInstanceIdentifier=RDSInstanceName, LogFileName=dbLog['LogFileName'],Marker=logFile['Marker'])
 					logFileData += logFile['LogFileData']
 			except Exception as e:
-				print "File download failed: ", e
+				print ("File download failed: ", e)
 				continue
 
 			logFileDataCleaned = logFileData.encode(errors='ignore')
-			logFileAsBytes = str.encode(logFileDataCleaned)
+			logFileAsBytes = str(logFileDataCleaned).encode()
 
 			# upload the log file to S3
 			objectName = RDSInstanceName + "/" + "backup_" + backupStartTime.isoformat() + "/" + dbLog['LogFileName']
@@ -142,22 +141,22 @@ def copy_logs_from_RDS_to_S3():
 				S3response = S3client.put_object(Bucket=S3BucketName, Key=objectName,Body=logFileAsBytes)
 				copiedFileCount += 1
 			except botocore.exceptions.ClientError as e:
-				print "Error writting object to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
+				print ("Error writting object to S3 bucket, S3 ClientError: " + e.response['Error']['Message'])
 				return
 			print("Uploaded log file %s to S3 bucket %s" % (objectName,S3BucketName))
 
-	print "Copied ", copiedFileCount, "file(s) to s3"
+	print ("Copied ", copiedFileCount, "file(s) to s3")
 
 	# Update the last written time in the config
 	if lastWrittenThisRun > 0:
 		try:
 			S3response = S3client.put_object(Bucket=S3BucketName, Key=configFileName, Body=str.encode(str(lastWrittenThisRun)))			
 		except botocore.exceptions.ClientError as e:
-			print "Error writting the config to S3 bucket, S3 ClientError: " + e.response['Error']['Message']
+			print ("Error writting the config to S3 bucket, S3 ClientError: " + e.response['Error']['Message'])
 			return
 		print("Wrote new Last Written file to %s in Bucket %s" % (configFileName,S3BucketName))
 
-	print "Log file export complete"
+	print ("Log file export complete")
 
 ###### START OF SCRIPT ####
 
