e75677c23a2cb286b5cfd255368a33f7e97de964
Vinícius Schmidt <vinics@amazon.com>
2021-11-04 14:10:25 +0000

    pg_stat_statements top10 lowest buffer hit ratio

---
+++ postgres/diag/sql/list-top10-lowest-buffer-hit-ratio-per-query.sql
@@ -0,0 +1,11 @@
+
+SELECT query
+      ,calls
+      ,total_exec_time
+      ,rows
+      ,100.0*shared_blks_hit/nullif(shared_blks_hit + shared_blks_read,0) AS hit_percent
+  FROM pg_stat_statements 
+ORDER BY total_exec_time DESC 
+LIMIT 5;
+
+
