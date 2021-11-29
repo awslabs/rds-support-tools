1cf912ed8e09c455fb5a4b365322e9fb9d9e53f5
Vinícius Schmidt <vinics@amazon.com>
2021-11-29 16:49:50 +0000

    Including back catalog and toast

--- postgres/diag/stats_snapshot/views/vw_statio_all_tables_history.sql
+++ postgres/diag/stats_snapshot/views/vw_statio_all_tables_history.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE VIEW vw_statio_all_tables_history AS
 WITH stat_history AS (
           SELECT  PSATH.*
             FROM   pg_statio_all_tables_history PSATH
-            WHERE  1=1
-              AND  ( PSATH.relname NOT like 'pg_%' AND  PSATH.schemaname NOT like 'information_schema' ) -- let system objects out of this
-              --AND  ( PSATH.relname like '%mitsta%' OR PSATH.relname IN ('') -- replace tablename by the actual name of the table
+             -- WHERE  1=1
+             --  AND  ( PSATH.relname NOT like 'pg_%' AND  PSATH.schemaname NOT like 'information_schema' ) -- let system objects out of this
+             -- AND  ( PSATH.relname like '%mitsta%' OR PSATH.relname IN ('') -- replace tablename by the actual name of the table
                /*
               AND  EXISTS
                 (      -- primary filter
