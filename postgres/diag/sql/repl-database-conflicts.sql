0db1f031a10536db7ae2c69033ec58cee799d5cd
Vinícius Schmidt <vinics@amazon.com>
2021-12-23 17:06:48 +0000

    Monitoring pg_stat_archiver and pg_database_conflicts

---
+++ postgres/diag/sql/repl-database-conflicts.sql
@@ -0,0 +1,3 @@
+
+-- https://www.postgresql.org/docs/current/monitoring-stats.html#MONITORING-PG-STAT-DATABASE-CONFLICTS-VIEW
+SELECT now(),* FROM pg_catalog.pg_stat_database_conflicts;
---
+++ postgres/diag/sql/stat-archiver.sql
@@ -0,0 +1,2 @@
+-- https://www.postgresql.org/docs/current/monitoring-stats.html#PG-STAT-ARCHIVER-VIEW
+select now(),* from pg_catalog.pg_stat_archiver ;
