ceb4d46ec7152b4a7ff25868aac234d26efd0bb1
Vinícius Schmidt <vinics@amazon.com>
2022-01-13 16:08:21 +0000

    Casting to bigint preventing errors for larger xmin

--- postgres/diag/sql/repl-xmin-from-activity.sql
+++ postgres/diag/sql/repl-xmin-from-activity.sql
@@ -1,1 +1,1 @@
-select now(),min(backend_xid::text::int), min(backend_xmin::text::int), max(backend_xid::text::int), max(backend_xmin::text::int) from pg_stat_activity;
+select now(),min(backend_xid::text::bigint), min(backend_xmin::text::bigint), max(backend_xid::text::bigint), max(backend_xmin::text::bigint) from pg_stat_activity;
