2e9ca439f2573900affe7ddd9572374b03d184d2
Vinícius Schmidt <vinics@amazon.com>
2023-01-30 11:26:14 +0000

    Removing DESC was missed from previous commit

--- postgres/diag/sql/list-top10-lowest-buffer-hit-ratio-per-query.sql
+++ postgres/diag/sql/list-top10-lowest-buffer-hit-ratio-per-query.sql
@@ -10,7 +10,7 @@ SELECT query
       ,rows
       ,100.0*shared_blks_hit/nullif(shared_blks_hit + shared_blks_read,0) AS hit_percent
   FROM pg_stat_statements 
-ORDER BY hit_percent DESC 
+ORDER BY hit_percent 
 LIMIT 10;
 
 
